2f3e4ec75e231e68c98123437dfe167fadbe3c3d
Patrick Rigg <patrickarigg@gmail.com>
2024-01-02 15:53:35 +0000

    first attempt at database seeding

--- App.tsx
+++ App.tsx
@@ -4,36 +4,38 @@ import { StyleSheet, Text, View } from "react-native";
 import Auth from "./components/Auth";
 import EmailForm from "./components/EmailForm";
 import { supabase } from "./lib/supabase";
+import Stories from "./components/Stories";
 
 export default function App() {
-    const [session, setSession] = useState<Session | null>(null);
+  const [session, setSession] = useState<Session | null>(null);
 
-    useEffect(() => {
-        supabase.auth.getSession().then(({ data: { session } }) => {
-            setSession(session);
-        });
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session);
+    });
 
-        supabase.auth.onAuthStateChange((_event, session) => {
-            setSession(session);
-        });
-    }, []);
+    supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session);
+    });
+  }, []);
 
-    console.log(session, "<--- session");
+  console.log(session, "<--- session");
 
-    return (
-        <View style={styles.container}>
-            <EmailForm />
-            <Auth />
-            <Text>{session?.user.id}</Text>
-        </View>
-    );
+  return (
+    <View style={styles.container}>
+      <EmailForm />
+      <Auth />
+      <Text>{session?.user.id}</Text>
+      <Stories />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: "#fff",
-        alignItems: "center",
-        justifyContent: "center",
-    },
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
 });
