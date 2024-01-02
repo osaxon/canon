02900ec288ac49e3a04ecdcd7dcd49b45fc5fbe7
Esther <11yekinie@gmail.com>
2024-01-02 16:14:03 +0000

    add navigation to sign in page and about page

--- App.tsx
+++ App.tsx
@@ -2,9 +2,21 @@ import HomePage from "./app/HomePage";
 import { Session } from "@supabase/supabase-js";
 import { useEffect, useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
+import * as React from 'react'
 import Auth from "./app/Auth";
 import EmailForm from "./app/EmailForm";
 import { supabase } from "./lib/supabase";
+import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import About from "./app/about";
+
+export type RootStackParams = {
+    Home: any;
+    SignIn: any;
+    About: any;
+}
+
+const Stack = createNativeStackNavigator<RootStackParams>();
 
 export default function App() {
     const [session, setSession] = useState<Session | null>(null);
@@ -22,14 +34,22 @@ export default function App() {
     console.log(session, "<--- session");
 
     return (
-        <View style={styles.container}>
-            <HomePage />
-            <EmailForm />
-            <Auth />
-            <Text>{session?.user.id}</Text>
-        </View>
+        <NavigationContainer>
+            <Stack.Navigator>
+                <Stack.Screen name="Home" component={HomePage} />
+                <Stack.Screen name="SignIn" component={EmailForm} />
+                <Stack.Screen name="About" component={About} />
+            </Stack.Navigator>
+        </NavigationContainer>
     );
 }
+
+            // <View style={styles.container}>
+            //     <HomePage />
+            //     <EmailForm />
+            //     <Auth />
+            //     <Text>{session?.user.id}</Text>
+            // </View>
 
 const styles = StyleSheet.create({
     container: {
