3a09324301e4fb5434562bc440f365203b8a6e9e
Patrick Rigg <patrickarigg@gmail.com>
2024-01-03 12:50:57 +0000

    adds google oauth login and stling to buttons

--- App.tsx
+++ App.tsx
@@ -4,50 +4,45 @@ import { Session } from "@supabase/supabase-js";
 import * as React from "react";
 import { useEffect, useState } from "react";
 import { StyleSheet } from "react-native";
-import Auth from "./app/Auth";
-import EmailForm from "./app/EmailForm";
-import Profile from "./app/Profile"
+import Profile from "./app/Profile";
 import { supabase } from "./lib/supabase";
-import { NavigationContainer } from '@react-navigation/native'
-import { createNativeStackNavigator } from '@react-navigation/native-stack'
 
 import HomePage from "./app/HomePage";
-
+import SignIn from "./app/SignIn";
 
 export type RootStackParams = {
-    Home: any;
-    SignIn: any;
-    Stories: any;
-    Profile: any;
+  Home: any;
+  SignIn: any;
+  Stories: any;
+  Profile: any;
 };
-
 
 const Stack = createNativeStackNavigator<RootStackParams>();
 
 export default function App() {
-    const [session, setSession] = useState<Session | null>(null);
+  const [session, setSession] = useState<Session | null>(null);
 
-    useEffect(() => {
-        supabase.auth.getSession().then(({ data: { session } }) => {
-            setSession(session);
-        });
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session);
+    });
 
-        supabase.auth.onAuthStateChange((_event, session) => {
-            setSession(session);
-        });
-    }, []);
+    supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session);
+    });
+  }, []);
 
-    console.log(session, "<--- session");
+  console.log(session, "<--- session");
 
-    return (
-        <NavigationContainer>
-            <Stack.Navigator>
-                <Stack.Screen name="Home" component={HomePage} />
-                <Stack.Screen name="SignIn" component={EmailForm} />
-                <Stack.Screen name="Profile" component={Profile} />
-            </Stack.Navigator>
-        </NavigationContainer>
-    );
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomePage} />
+        <Stack.Screen name="SignIn" component={SignIn} />
+        <Stack.Screen name="Profile" component={Profile} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
 
 // <View style={styles.container}>
@@ -58,10 +53,10 @@ export default function App() {
 // </View>
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: "#fff",
-        alignItems: "center",
-        justifyContent: "center",
-    },
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
 });
