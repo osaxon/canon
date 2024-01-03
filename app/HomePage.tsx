2feab808893ba37bfa0b031300f9eae697f18f97
Oliver Saxon <oliverrsaxon@gmail.com>
2024-01-03 13:24:16 +0000

    adds supabase edge function test and component to root of app

--- app/HomePage.tsx
+++ app/HomePage.tsx
@@ -1,29 +1,37 @@
-import { Text, TouchableOpacity, View } from 'react-native';
-import { StackParams } from '../App';
-import { NativeStackNavigationProp } from '@react-navigation/native-stack'
-import {useNavigation} from '@react-navigation/core'
+import { useNavigation } from "@react-navigation/core";
+import { NativeStackNavigationProp } from "@react-navigation/native-stack";
+import { Text, TouchableOpacity, View } from "react-native";
+import { StackParams } from "../App";
+import HelloWorld from "../components/HelloWorld";
 
 export default function HomePage() {
-  const navigation = useNavigation<NativeStackNavigationProp<StackParams>>()
-  return (
-    <>
-      <View>
-      <TouchableOpacity onPress={() => navigation.push("Home")}>
-          <Text>Home</Text>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={() => navigation.navigate("SignIn")}>
-          <Text>Sign In</Text>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={() => navigation.navigate('Profile', {user_id: 1})}>
-          <Text>Profile</Text>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={() => navigation.navigate('Stories')}>
-          <Text>Stories</Text>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={() => navigation.navigate('Users')}>
-          <Text>Users</Text>
-        </TouchableOpacity>
-        </View>
-    </>
-  );
+    const navigation = useNavigation<NativeStackNavigationProp<StackParams>>();
+    return (
+        <>
+            <View>
+                <TouchableOpacity onPress={() => navigation.push("Home")}>
+                    <Text>Home</Text>
+                </TouchableOpacity>
+                <TouchableOpacity onPress={() => navigation.navigate("SignIn")}>
+                    <Text>Sign In</Text>
+                </TouchableOpacity>
+                <TouchableOpacity
+                    onPress={() =>
+                        navigation.navigate("Profile", { user_id: 1 })
+                    }
+                >
+                    <Text>Profile</Text>
+                </TouchableOpacity>
+                <TouchableOpacity
+                    onPress={() => navigation.navigate("Stories")}
+                >
+                    <Text>Stories</Text>
+                </TouchableOpacity>
+                <TouchableOpacity onPress={() => navigation.navigate("Users")}>
+                    <Text>Users</Text>
+                </TouchableOpacity>
+                <HelloWorld />
+            </View>
+        </>
+    );
 }
---
+++ components/HelloWorld.tsx
@@ -0,0 +1,29 @@
+import { useEffect, useState } from "react";
+import { View } from "react-native";
+import { Text } from "react-native-elements";
+import { supabase } from "../lib/supabase";
+
+export default function HelloWorld() {
+    const [msg, setMsg] = useState<{ message: string } | null>(null);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        const fetchMsg = async () => {
+            const { data, error } = await supabase.functions.invoke(
+                "hello-world",
+                { body: { name: "from the Supabase Edge!" } }
+            );
+
+            if (error) setError(error);
+
+            setMsg(data);
+        };
+        fetchMsg();
+    }, []);
+
+    return (
+        <View>
+            <Text>{JSON.stringify(msg, null, 2)}</Text>
+        </View>
+    );
+}
