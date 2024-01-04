56748682738db0769a3f115764289b917af316eb
Oliver Saxon <oliverrsaxon@gmail.com>
2024-01-04 10:24:41 +0000

    adds generateimage component WIP

---
+++ components/GenerateImage.tsx
@@ -0,0 +1,21 @@
+import { useQuery } from "@tanstack/react-query";
+import { Text, View } from "react-native";
+import { supabase } from "../lib/supabase";
+
+const generateImageFromPrompt = async () => {
+    const { data, error } = await supabase.functions.invoke("generate-image");
+    return { data, error };
+};
+
+export default function GenerateImage() {
+    const { data } = useQuery({
+        queryKey: ["generated-image"],
+        queryFn: generateImageFromPrompt,
+    });
+
+    return (
+        <View>
+            <Text>{JSON.stringify(data, null, 2)}</Text>
+        </View>
+    );
+}
