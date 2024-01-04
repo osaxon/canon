5c2288327734a38ffbccd251e5674f1fb1b5dc95
Sailynx-Zenefer <siderealism.space@gmail.com>
2024-01-04 18:56:56 +0000

    formatting

--- app/StoryCard.tsx
+++ app/StoryCard.tsx
@@ -1,57 +1,51 @@
 import { useNavigation } from "@react-navigation/core";
 import { NativeStackNavigationProp } from "@react-navigation/native-stack";
 import { StackParams } from "../App";
-import {
-  StyleSheet,
-  TouchableOpacity,
-  View, Text,Image
-} from "react-native";
+import { StyleSheet, TouchableOpacity, View, Text, Image } from "react-native";
 
-import {Avatar} from "react-native-elements"
+import { Avatar } from "react-native-elements";
 
-const defaultUser = require("../assets/user.png")
-
+const defaultUser = require("../assets/user.png");
 
 const styles = StyleSheet.create({
   image: {
-    maxWidth:"100%",
-    width:1000,
-    maxHeight:"100%",
-    height:"auto",
-    borderRadius:10,
-    aspectRatio:1,
+    maxWidth: "100%",
+    width: 1000,
+    maxHeight: "100%",
+    height: "auto",
+    borderRadius: 10,
+    aspectRatio: 1,
   },
-  text :{
-    paddingLeft:10
+  text: {
+    paddingLeft: 10,
   },
   storyCard: {
-    boxSixing:"border-box",
-    display:"flex",
-    flexDirection:"column",
-    alignContent:"center",
-    justifyContent:"center",
+    boxSixing: "border-box",
+    display: "flex",
+    flexDirection: "column",
+    alignContent: "center",
+    justifyContent: "center",
     aspectRatio: 1,
-    minWidth:"50%",
+    minWidth: "50%",
     width: "100%",
-    maxWidth:500,
-    height:"auto",
-    maxheight:"100%",
+    maxWidth: 500,
+    height: "auto",
+    maxheight: "100%",
     marginTop: 60,
     marginBottom: 100,
-    margin:"auto",
+    margin: "auto",
   },
   avatarBox: {
-    flexDirection:"row",
-    alignContent:"center",
-    justifyContent:"center",
-    backgroundColor:"lightgrey",
-    borderRadius:10
-  }
-
+    flexDirection: "row",
+    alignContent: "center",
+    justifyContent: "center",
+    backgroundColor: "lightgrey",
+    borderRadius: 10,
+  },
 });
 
 interface StoryCardProps {
-  storyData:{
+  storyData: {
     id: number;
     story_id: number;
     user_id: number | null;
@@ -59,10 +53,20 @@ interface StoryCardProps {
     image_url: string | null;
     comment_count: number | null;
     votes: number | null;
-  }
+  };
 }
 
-const StoryCard = ({ storyData : {id,story_id, user_id, created_at, image_url, comment_count, votes }} : StoryCardProps) => {
+const StoryCard = ({
+  storyData: {
+    id,
+    story_id,
+    user_id,
+    created_at,
+    image_url,
+    comment_count,
+    votes,
+  },
+}: StoryCardProps) => {
   const navigation = useNavigation<NativeStackNavigationProp<StackParams>>();
   return (
     <>
@@ -70,26 +74,29 @@ const StoryCard = ({ storyData : {id,story_id, user_id, created_at, image_url, c
         <TouchableOpacity
           onPress={() => navigation.navigate("StoryAdd", { story_id })}
         >
-          <Image style={styles.image} source={{ uri: image_url!}} />
+          <Image style={styles.image} source={{ uri: image_url! }} />
         </TouchableOpacity>
-        <Text style={styles.text}>{`${comment_count} comments, ${votes} votes`}</Text>
+        <Text
+          style={styles.text}
+        >{`${comment_count} comments, ${votes} votes`}</Text>
         <View style={styles.avatarBox}>
-        <Avatar
-    size={"medium"}
-    rounded
-    containerStyle={{
-      borderColor: 'grey',
-      borderStyle: 'solid',
-      borderWidth: 1,
-    }}
-    source={defaultUser}
-  />
-          <Text style={styles.text}>{`${user_id} posted on ${created_at}`}</Text>
+          <Avatar
+            size={"medium"}
+            rounded
+            containerStyle={{
+              borderColor: "grey",
+              borderStyle: "solid",
+              borderWidth: 1,
+            }}
+            source={defaultUser}
+          />
+          <Text
+            style={styles.text}
+          >{`${user_id} posted on ${created_at}`}</Text>
         </View>
-
       </View>
     </>
   );
-}
+};
 
-export default StoryCard
+export default StoryCard;
