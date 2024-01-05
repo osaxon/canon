0679cd2b5e7fe0f28259e21c000809d802a2d11b
Esther <11yekinie@gmail.com>
2024-01-05 12:36:15 +0000

    updates database accordingly

--- app/Profile.tsx
+++ app/Profile.tsx
@@ -3,16 +3,54 @@ import {
   StyleSheet,
   Text,
   View,
-  Button,
   ScrollView,
+  TouchableOpacity,
 } from "react-native";
 import React from "react";
 import * as ImagePicker from "expo-image-picker";
 import * as FileSystem from "expo-file-system";
 import { supabase } from "../lib/supabase";
-import { decode } from 'base64-arraybuffer'
+import { decode } from "base64-arraybuffer";
+import { useState, useEffect } from "react";
+import { Session } from "@supabase/supabase-js";
 
 export default function Profile() {
+  const [session, setSession] = useState<Session | null>(null);
+  const [avatarUrl, setAvatarUrl] = useState<string | null>(null);
+
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session);
+    });
+
+    supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session);
+    });
+  }, []);
+
+  useEffect(() => {
+    async function fetchAvatarUrl() {
+      if (session?.user.id) {
+        const { data, error } = await supabase
+          .from("profiles")
+          .select("avatar_url")
+          .eq("id", session.user.id)
+          .single();
+
+        if (error) {
+          console.error("Error fetching avatar URL:", error);
+        } else if (data && data.avatar_url) {
+          setAvatarUrl(data.avatar_url);
+        }
+      }
+    }
+    fetchAvatarUrl();
+  }, [session]);
+
+  const defaultProfile = avatarUrl;
+  const filePath = `${session?.user.user_metadata.user_name}_avatars/${
+    session?.user.user_metadata.user_name
+  }_${new Date().getTime()}.jpg`;
   async function pickImage(bucketName: string, filePath: string) {
     try {
       let result = await ImagePicker.launchImageLibraryAsync({
@@ -37,15 +75,16 @@ export default function Profile() {
               const { data, error } = await supabase.storage
                 .from(bucketName)
                 .upload(filePath, decode(`${base64}`), {
-                  contentType: 'image/jpeg'
+                  contentType: "image/jpeg",
+                  upsert: true,
                 });
 
               if (error) {
-                alert("Error uploading file: " + error.message); 
+                alert("Error uploading file: " + error.message);
                 return;
               }
 
-              alert("File uploaded successfully" + JSON.stringify(data));
+              alert("File uploaded successfully");
             } catch (error) {
               console.error("Error processing the file: ", error);
             }
@@ -60,63 +99,145 @@ export default function Profile() {
     } catch (error) {
       console.error("Error during image picking or uploading: ", error);
     }
+
+    const { data } = supabase.storage.from(bucketName).getPublicUrl(filePath);
+
+    if (data) {
+      const uploadedImageUrl = data.publicUrl;
+      setAvatarUrl(uploadedImageUrl);
+
+      const { error } = await supabase
+        .from("profiles")
+        .update({ avatar_url: uploadedImageUrl })
+        .eq("id", session?.user.id);
+
+      if (error) {
+        console.error("Error updating profile: ", error);
+      } else {
+        setAvatarUrl(uploadedImageUrl);
+        console.log(
+          "Avatar URL updated successfully: ",
+          avatarUrl,
+          uploadedImageUrl
+        );
+      }
+    } else {
+      console.error("Unable to get the public URL");
+    }
   }
 
   return (
     <>
-      <Image style={styles.profile} source={require("../assets/dalle1.png")} />
-      <Button
-        onPress={() => pickImage("avatars", `user_avatars/user123.jpg`)}
-        title="edit"
-      />
-      <View>
-        <Text>Points</Text>
-        <Text>12</Text>
-      </View>
-      <View>
-        <Text>Following</Text>
-        <Text>5</Text>
-      </View>
-      <View>
-        <Text>UserName</Text>
-        <Text>Story Additions</Text>
-        <Text>Story Creations</Text>
-      </View>
-      <Button title="Follow" />
-      <View></View>
-      <View>
-        <Text>Latest Stories</Text>
-        <ScrollView>
-          <Image
-            style={styles.stories}
-            source={require("../assets/icon.png")}
-          />
-          <Image
-            style={styles.stories}
-            source={require("../assets/icon.png")}
-          />
-          <Image
-            style={styles.stories}
-            source={require("../assets/icon.png")}
-          />
-          <Image
-            style={styles.stories}
-            source={require("../assets/icon.png")}
-          />
-        </ScrollView>
+      <View style={styles.container}>
+        <Image
+          style={styles.profile}
+          source={{ uri: avatarUrl || defaultProfile }}
+        />
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => pickImage("avatars", filePath)}
+        >
+          <Text style={styles.buttonText}>Edit</Text>
+        </TouchableOpacity>
+        <View style={styles.userInfoSection}>
+          <Text style={styles.userInfoText}>Following: 5</Text>
+          <Text
+            style={styles.userInfoText}
+          >{`${session?.user.user_metadata.user_name}`}</Text>
+          {/* <Text style={styles.userInfoText}>Story Additions</Text>
+          <Text style={styles.userInfoText}>Story Creations</Text> */}
+        </View>
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>Follow</Text>
+        </TouchableOpacity>
+        <Text style={styles.titleText}>Latest Stories</Text>
+        <View style={styles.storyContainer}>
+          {/* <ScrollView showsHorizontalScrollIndicator={false}> */}
+            <Image
+              style={styles.stories}
+              source={require("../assets/icon.png")}
+            />
+            <Image
+              style={styles.stories}
+              source={require("../assets/icon.png")}
+            />
+            <Image
+              style={styles.stories}
+              source={require("../assets/icon.png")}
+            />
+          {/* </ScrollView> */}
+        </View>
       </View>
     </>
   );
 }
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#f0f0f0",
+    alignItems: "center",
+    justifyContent: "flex-start",
+    width: '100%',
+  },
   profile: {
-    width: 100,
-    height: 100,
-    borderRadius: 50,
+    width: 120,
+    height: 120,
+    borderRadius: 60,
+    borderWidth: 2,
+    borderColor: "#333",
+    marginTop: 10,
+    marginBottom: 10,
+  },
+  button: {
+    backgroundColor: "#0095f6",
+    paddingVertical: 8,
+    paddingHorizontal: 15,
+    borderRadius: 5,
+    marginTop: 10,
+  },
+  buttonText: {
+    color: "#fff",
+    fontWeight: "600",
+  },
+  userInfoSection: {
+    width: "100%",
+    alignItems: "flex-start",
+    paddingHorizontal: 15,
+    marginBottom: 20,
+  },
+  userInfoText: {
+    fontSize: 18,
+    color: "#333",
+    margin: 3,
   },
   stories: {
-    width: 150,
-    height: 200,
+    width: 122,
+    height: 122,
+    borderRadius: 3,
+    borderColor: "#333",
+    borderWidth: 1,
+    margin: 0,
+  },
+  scrollView: {
+    width: "100%",
+    marginTop: 10,
+  },
+  storyContainer: {
+    flex: 1,
+    flexDirection: "row",
+    flexWrap: "wrap",
+    justifyContent: "flex-start",
+    width: "100%",
+    paddingHorizontal: 10,
+  },
+  titleText: {
+    fontSize: 18,
+    fontWeight: "bold",
+    color: "#262626",
+    alignSelf: "flex-start",
+    paddingHorizontal: 15,
+    marginBottom: 20,
+    marginTop: 20,
   },
 });
