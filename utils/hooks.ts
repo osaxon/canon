652d32eea0b85b8ed06d94de00372b7e20e2b20f
Oliver Saxon <oliverrsaxon@gmail.com>
2024-01-05 14:07:32 +0000

    adds hooks for storing the generated image in the database

--- utils/hooks.ts
+++ utils/hooks.ts
@@ -1,4 +1,5 @@
 import { supabase } from "../lib/supabase";
+import { Tables } from "../types/database";
 import { StoreImageProps } from "../types/functions";
 import { storeImage } from "./supabase";
 import { useQuery, useMutation } from "@tanstack/react-query";
@@ -29,13 +30,56 @@ export const useGetComments = () => {
     });
 };
 
-export const useStoreImage = ({
+/**
+ * Gets comments for a single story
+ *
+ * @param {string} storyId the id of the story
+ * @returns array of comments
+ */
+export const useGetStoryComments = (storyId: string) => {
+    return useQuery({
+        queryKey: ["story_comments", storyId],
+        queryFn: async () => {
+            const { data } = await supabase
+                .from("comments")
+                .select("*")
+                .eq("story_item_id", storyId)
+                .throwOnError();
+            return data || [];
+        },
+    });
+};
+
+/**
+ * Uploads an image to storage and saves to a story item in the db
+ *
+ * @param {string} base64 the image to be stored - base64 format
+ * @param {string} fileName the name of the file
+ * @param {string} filePath the path to store the file
+ * @param {string} storyItemId the id of the story item the image is related to
+ * @returns the story item
+ */
+export const useStoreImageForStory = ({
     base64,
     fileName,
     filePath,
-}: StoreImageProps) => {
+    storyItemId,
+}: StoreImageProps & { storyItemId: string }) => {
     return useMutation({
         mutationKey: ["store-image", fileName, filePath],
-        mutationFn: () => storeImage({ base64, fileName, filePath }),
+        mutationFn: async () => {
+            // 1. store the image to the storage bucket
+            const img = await storeImage({ base64, fileName, filePath });
+
+            // 2. update the story_item in the db
+            const { data } = await supabase
+                .from("story_items")
+                .update({ image_url: img.path })
+                .eq("id", storyItemId)
+                .select("*")
+                .throwOnError();
+
+            return data || [];
+        },
     });
 };
